31a7553ab8028f36f1cb87ca858ff88b7cebbfcd
ddd <eladevrim65@gmail.com>
2025-03-25 16:31:45 +0300

    gadf
    adfgafd

--- inc/config.php
+++ inc/config.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'mobile.class.php';
-$title = "jojobet 1016 - Jojobet - Jojobet™ » Jojobet 1016 com giriş - jojobet1016 com - Jojobet 1016 Giriş - Jojobet Güncel";
+$title = "Pusulabet 879 - Pusulabet - Pusulabet 879 com giriş - pusulabet879 com ";
 ob_start();
 session_start();
 error_reporting(0);
@@ -12,8 +12,8 @@ const DB_HOST = "localhost";
 const DB_LOGIN = "admin_pae";
 const DB_PASS = "4mc_z61S4";
 const DB_NAME = "admin_pae";
-$guncel = 'jojobet1016';
-$guncel2 = 'jojobet1017';
+$guncel = 'pusulabet879';
+$guncel2 = 'pusulabet880';
 
 // Connecting Database
 $db = new Mysqli(DB_HOST, DB_LOGIN, DB_PASS, DB_NAME);
